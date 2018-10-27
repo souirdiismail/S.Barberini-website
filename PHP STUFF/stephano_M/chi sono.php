85ab0c8972052ad1526f56421282d6c54fa36247
Elhassan-mendil <36772403+Elhassan-mendil@users.noreply.github.com>
2018-10-27 18:18:09 +0100

    here's it

---
+++ PHP STUFF/stephano_M/chi sono.php
@@ -0,0 +1,116 @@
+<?php require "header.php"?>
+
+     <section class="content container">
+        <div class="profile-photo ">
+            <span class="card-img">
+                <img src="Assets\profile.png" alt="" class="portraits">
+                <img src="Assets\Rectangle.svg" alt="" class="stroke">
+            </span>
+        </div>
+        <div class="coach">
+            <span class="name-coach">
+                Stefano Barberini
+            </span>
+            <br>
+            <br>
+            <span class="spec-coach">
+                coach
+            </span>
+            
+        </div>
+        <br>
+        <div class="text">
+            <article>
+                <center>Ciao Mi chiamo Stefano Barberini e mi presento.</center>    
+                <br>
+                <br>
+                <center>Ho 40 anni e da 4 anni mi occupo di nutrizione.</center>
+                <br>
+                <br>
+                Esattamente 5 anni fa, guardandomi in una foto scattata in spiaggia mentre giocavo a racchettoni mi
+                vidi terribilmente fuori forma.
+                <br><br>
+                I pantaloni mi stanno stretti in vita e ogni volta che dovevo andare al mare mi vergognavo e mettermi
+                in costume.
+                <br><br>
+                <strong>Vidi quella foto e dissi:”BASTA!”</strong>
+                <br><br>
+                Non avevo idea di come mettermi in forma perché non sono mai stato un gran sportivo e a dire il vero
+                non sapevo da dove iniziare.
+                <br><br>
+                Andai come prima cosa in palestra, mi rivolsi all’istruttore, che mi fece una “scheda” copia incolla.
+                Qualche esercizio spiegato velocemente qua e la ed una pacca sulla spalla.
+                <br><br>
+                Ogni volta che entravo in quella palestra mi sentivo male, giudicato da quelli che avevano un bicipite
+                grande come la mia testa e squadrato da testa a piedi perché non avevo un abbigliamento da palestrato
+                navigato.
+                <br><br>
+                Nonostante andassi 2/3 volte alla settimana non ottieni risultati, fisicamente mi sentivo uguale e la
+                pancia non accennava ad andare via.
+                <br><br>
+                Capii che forse avevo qualche problema di alimentazione allora chiesi consigli all’istruttore della
+                palestra che mi consigliò di mangiare tanto pollo, tanto riso, tanto tonno e i legumi.
+                <br><br>
+                Dopo un altro periodo di allenamento e di nutrizione a base di pollo, tonno, riso e fagioli avevo il
+                morale a zero e la pancia sempre lì.
+                <br><br>
+                A quel punto ero disperato e una mia amica giocatrice di pallavolo e Campionessa del mondo mi
+                disse:”prova Herbalife, io l’ho usata quando ero in Nazionale e ti devo dire che mi ero trovata molto
+                bene” Sono sempre stato molto scettico su tutto quello che era il mondo degli integratori perché non li
+                conoscevo, ma quando una giocatrice professionista conosciuta in tutto il mondo ti consiglia qualcosa,
+                be’ prendi il consiglio e cerchi di farne tesoro.
+                <br><br>
+                Non sapendo a chi rivolgermi a Modena, io sono di Modena, perché non c’era nessuno, mi ricordai che in
+                un viaggio fatto a Miami avevo conosciuto un ragazzo che lavorava per Herbalife.
+                <br><br>
+                L’ho contattato e mi sono fatto dare da subito i prodotti per iniziare.
+                <br><br>
+                Ero entusiasta, mi aveva spiegato tante cose e riempito di tanti consigli. Iniziai subito a cambiare la
+                mia colazione classica che era the con biscotti e già con quella sentii da subito una grande energia la
+                mattina.
+                <br><br>
+                Non arrivavo più affamato come un lupo a pranzo e la mia pancia era molto più sgonfia.
+                <br><br>
+                Mi sentivo bene e a differenza di prima vedevo i risultati arrivare e più arrivavano e più ero felice.
+                Continuai con il mio solito lavoro, ero responsabile Marketing di una squadra di pallavolo di Serie A.
+                <br><br>
+                Dopo poco tempo ottieni il mio risultato e mettermi in costume o senza maglietta non era più un
+                problema e continuai con la mia colazione Herbalife tutte le mattine. Mi piaceva. Era buona, veloce da
+                preparare e mi sentivo davvero carico.
+                <br><br>
+                Qualche mese più tardi però mio fratello ebbe un problema di salute ed essendo in forte sovrappeso il
+                suo cardiologo lo obbligo a perdere peso.
+                <br><br>
+                Andò da una nutrizionista, ma nonostante ciò perdeva peso molto a rilento e i sacrifici erano
+                importanti.Un giorno mio fratello venne a casa mia e vide nella ia cucina i barattoli dei frullati
+                Herbalife che io usavo giornalmente.Mi chiese se funzionassero, gli parlai dei risultati che avevano
+                dato e me ed in men che non si dica anche lui iniziò coi frullati Herbalife.
+                <br><br>
+                Anche mio fratello cambiò il suo stile di vita, inizio ad idratarsi bene e con l’utilizzo dei Prodotti
+                Herbalife perse la bellezza di 34 Kilogrammi. Proprio così, 34Kg!!! Incredibile.
+                <br><br>
+                Inutile dirvi la sua felicità e lo stupore dei suoi amici, colleghi di lavoro e familiari nel vedere
+                questa trasformazione giorno dopo giorno, mese dopo mese.Senza accorgermene mio fratello aveva “dato il
+                LA” alla mia nuova vita oltre che alla sua.
+                <br><br>
+                Sì perché da lì in poi tanti suoi amici iniziarono a chiedermi consigli e i prodotti per poter
+                dimagrire e ottenere risultati.Ho partecipato ad un centinaio di Corsi di formazione e da quella che
+                poteva essere una passione è diventata una vera e propria attività.
+                <br><br>
+                Ora abbiamo una palestra e diversi centri sparsi in tutta Italia e clienti anche fino negli Stati
+                Uniti, in Russia, Inghilterra e tanti altri paesi.
+                <br><br>
+                Più di mille clienti soddisfatti che usano i prodotti Herbalife e che seguono i miei ed i nostri
+                consigli.
+                <br><br>
+                Per me non parlano i prodotti venduti o la rete commerciale che ho creato ma parlano i risultati delle
+                persone che Personalmente ho seguito e che hanno seguito i Coach che ho Personalmente Formato.Benvenuto
+                mel mio Mondo e nel mondo del Wellness!!
+                <br><br>
+                <strong>Barberini</strong>
+            </article>
+        </div>
+    </section>
+
+
+<?php require "footer.php"?>
