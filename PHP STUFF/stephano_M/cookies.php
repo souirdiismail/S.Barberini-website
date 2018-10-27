b4a6105eaa4300ee348ba1f93662d6db2d94429e
Atheem <36827002+Atheem@users.noreply.github.com>
2018-10-28 00:12:41 +0100

    up

---
+++ PHP STUFF/stephano_M/cookies.php
@@ -0,0 +1,201 @@
+<?php require "header.php"?>
+
+<div class="container cookies_section">
+<div >
+Questo sito web utilizza i cookie. Utilizziamo i cookie per personalizzare contenuti ed annunci, per fornire funzionalità dei social media e per analizzare il nostro traffico. Condividiamo inoltre informazioni sul modo in cui utilizza il nostro sito con i nostri partner che si occupano di analisi dei dati web, pubblicità e social media, i quali potrebbero combinarle con altre informazioni che ha fornito loro o che hanno raccolto dal suo utilizzo dei loro servizi. Acconsenta ai nostri cookie se continua ad utilizzare il nostro sito web.
+
+I cookie sono piccoli file di testo che possono essere utilizzati dai siti web per rendere più efficiente l'esperienza per l'utente.
+
+La legge afferma che possiamo memorizzare i cookie sul suo dispositivo se sono strettamente necessari per il funzionamento di questo sito. Per tutti gli altri tipi di cookie abbiamo bisogno del suo permesso.
+
+Questo sito utilizza diversi tipi di cookie. Alcuni cookie sono collocate da servizi di terzi che compaiono sulle nostre pagine.
+
+In qualsiasi momento è possibile modificare o revocare il proprio consenso dalla Dichiarazione dei cookie sul nostro sito Web.
+
+Scopra di più su chi siamo, come può contattarci e come trattiamo i dati personali nella nostra Informativa sulla privacy.
+
+Il suo consenso si applica ai seguenti siti web: www.stefanobarberini.com
+<br>
+
+Il suo stato attuale: Usa solo i cookie necessari . <span><strong>Modifica il suo consenso</strong></span>
+
+<br>
+Dichiarazione Cookie aggiornata l'ultima volta il 04/10/2018 da <a href="https://www.cookiebot.com/en/"><strong>Cookiebot</strong></a> :
+</div>
+<br> <br>
+
+    <div class="container">
+        <strong>Necessario (3)</strong>
+        <br><br>
+I cookie necessari aiutano a contribuire a rendere fruibile un sito web abilitando le funzioni di base come la navigazione della pagina e l'accesso alle aree protette del sito.
+ Il sito web non può funzionare correttamente senza questi cookie.
+ <br><br>
+ <table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Nome</th>
+      <th scope="col">Fornitore</th>
+      <th scope="col">Scopo</th>
+      <th scope="col">Scadenza</th>
+      <th scope="col">Tipo</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">CookieConsent</th>
+      <td>stefanobarberini-club24hl.com</td>
+      <td>Memorizza lo stato del consenso ai cookie dell'utente per il dominio corrente</td>
+      <td>1 anno</td>
+      <td>HTTP Cookie</td>
+    </tr>
+    <tr>
+      <th scope="row">csrftoken</th>
+      <td>instagram.com</td>
+      <td>Aiuta a prevenire gli attacchi CSRF (Cross-Site Request Forgery).</td>
+      <td>1 anno</td>
+      <td>HTTP Cookie</td>
+    </tr>
+    <tr>
+</tbody>
+</table>
+</div>
+
+
+
+<div class="container">
+    <strong>Marketing (14)</strong>
+    <br><br>
+    <p>
+    I cookie per il marketing vengono utilizzati per monitorare i visitatori nei siti web. L'intento è quello di visualizzare annunci pertinenti e coinvolgenti per il singolo utente 
+    e quindi quelli di maggior valore per gli editori e gli inserzionisti terzi.
+    </p><br><br>
+    
+
+
+
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Nome</th>
+      <th scope="col">Fornitore</th>
+      <th scope="col">Scopo</th>
+      <th scope="col">Scadenza</th>
+      <th scope="col">Tipo</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">GPS</th>
+      <td>youtube.com</td>
+      <td>Registra un ID univoco sui dispositivi mobile per consentire il tracciamento sulla base della posizione geografica GPS.</td>
+      <td>Session</td>
+      <td>HTTP Cookie</td>
+    </tr>
+    <tr>
+      <th scope="row">IDE</th>
+      <td>doubleclick.net</td>
+      <td>Utilizzato da Google DoubleClick per registrare e produrre resoconti sulle azioni dell'utente sul sito dopo aver visualizzato o cliccato una delle pubblicità dell'inserzionista al fine di misurare l'efficacia di una pubblicità e presentare pubblicità mirata all'utente.	</td>
+      <td>1 anno</td>
+      <td>HTTP Cookie</td>
+    </tr>
+    <tr>
+      <th scope="row">PREF</th>
+      <td>youtube.com</td>
+      <td>Registra un ID univoco utilizzato da Google per statistiche legate a come il visitatore utilizza i video YouTube su diversi siti internet.</td>
+      <td>8 mesi</td>
+      <td>HTTP Cookie</td>
+    </tr>
+    <tr>
+      <th scope="row">test_cookie</th>
+      <td>doubleclick.net</td>
+      <td>RUtilizzato per verificare se il browser dell'utente supporta i cookie.</td>
+      <td>Session</td>
+      <td>HTTP Cookie</td>
+    </tr>
+
+    <tr>
+      <th scope="row">VISITOR_INFO1_LIVE</th>
+      <td>youtube.com</td>
+      <td>Prova a stimare la velocità della connessione dell'utente su pagine con video YouTube integrati.</td>
+      <td>179 giorn</td>
+      <td>HTTP Cookie</td>
+    </tr>
+
+    <tr>
+      <th scope="row">YSC</th>
+      <td>youtube.com</td>
+      <td>Registra un ID univoco per statistiche legate a quali video YouTube sono stati visualizzati dall'utente.</td>
+      <td>Session</td>
+      <td>HTTP Cookie</td>
+    </tr>
+    <tr>
+      <th scope="row">yt.innertube::nextId</th>
+      <td>youtube.com</td>
+      <td>In attesa</td>
+      <td>Persistent</td>
+      <td>HTML Local Storage</td>
+    </tr>
+    <tr>
+      <th scope="row">yt.innertube::requests</th>
+      <td>youtube.com</td>
+      <td>In attesa</td>
+      <td>Persistent</td>
+      <td>HTML Local Storage</td>
+    </tr>
+    <tr>
+      <th scope="row">yt-remote-cast-installed</th>
+      <td>youtube.com</td>
+      <td>Memorizza le preferenze del lettore video dell'utente usando il video YouTube incorporato</td>
+      <td>Session</td>
+      <td>HTML Local Storage</td>
+    </tr>
+    <tr>
+      <th scope="row">yt-remote-connected-devices</th>
+      <td>youtube.com</td>
+      <td>Memorizza le preferenze del lettore video dell'utente usando il video YouTube incorporato</td>
+      <td>Persistent</td>
+      <td>HTML Local Storage</td>
+    </tr>
+    <tr>
+      <th scope="row">yt-remote-device-id</th>
+      <td>youtube.com</td>
+      <td>Memorizza le preferenze del lettore video dell'utente usando il video YouTube incorporato</td>
+      <td>Session</td>
+      <td>HTML Local Storage</td>
+    </tr>
+    <tr>
+      <th scope="row">yt-remote-fast-check-period</th>
+      <td>youtube.com</td>
+      <td>Memorizza le preferenze del lettore video dell'utente usando il video YouTube incorporato	</td>
+      <td>Session</td>
+      <td>HTML Local Storage</td>
+    </tr>
+    <tr>
+      <th scope="row">yt-remote-fast-check-period</th>
+      <td>youtube.com</td>
+      <td>Memorizza le preferenze del lettore video dell'utente usando il video YouTube incorporato	</td>
+      <td>Session</td>
+      <td>HTML Local Storage</td>
+    </tr>
+    <tr>
+      <th scope="row">yt-remote-session-app</th>
+      <td>youtube.com</td>
+      <td>Memorizza le preferenze del lettore video dell'utente usando il video YouTube incorporato	</td>
+      <td>Session</td>
+      <td>HTML Local Storage</td>
+    </tr>
+    <tr>
+      <th scope="row">yt-remote-session-name</th>
+      <td>youtube.com</td>
+      <td>Memorizza le preferenze del lettore video dell'utente usando il video YouTube incorporato	</td>
+      <td>Session</td>
+      <td>HTML Local Storage</td>
+    </tr>
+    
+</tbody>
+</table>
+</div>
+</div>
+
+
+<?php require "footer.php"?>
