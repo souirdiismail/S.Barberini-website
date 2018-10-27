b2aa8a2577eaf9391e26042869fb3348cd999af6
Elhassan-mendil <36772403+Elhassan-mendil@users.noreply.github.com>
2018-10-28 00:46:36 +0100

    ...

--- PHP STUFF/stephano_M/footer.php
+++ PHP STUFF/stephano_M/footer.php
@@ -1,57 +1,56 @@
 <!-- Footer -->
-    <section id="footer">
-    	<hr class="my-4" >
-        <div class="container">
-            <div class="row text-center text-xs-center text-sm-left text-md-left">
-                <div class="col-xs-12 col-sm-4 col-md-4">
-                    <h5>Sections</h5>
-                    <ul class="list-unstyled quick-links">
-                        <li><a href="index"><i class="fa fa-angle-double-right"></i>Home</a></li>
-                        <li><a href="<?php if($pageName=='index'){echo '#Prodotto';}else{ echo 'index#Prodotto';}?>"><i class="fa fa-angle-double-right"></i>Productto</a></li>
-                        <li><a href="chi sono"><i class="fa fa-angle-double-right"></i>About</a></li>
-                        <li><a href="Video"><i class="fa fa-angle-double-right"></i>Blog</a></li>
+<section id="footer">
+    <hr class="my-4">
+    <div class="container">
+        <div class="row text-center text-xs-center text-sm-left text-md-left">
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <h5>Sections</h5>
+                <ul class="list-unstyled quick-links">
+                    <li><a href="index"><i class="fa fa-angle-double-right"></i>Home</a></li>
+                    <li><a href="<?php if($pageName=='index'){echo '#Prodotto';}else{ echo 'index#Prodotto';}?>"><i class="fa fa-angle-double-right"></i>Productto</a></li>
+                    <li><a href="chi sono"><i class="fa fa-angle-double-right"></i>About</a></li>
+                    <li><a href="Video"><i class="fa fa-angle-double-right"></i>Blog</a></li>
 
-                    </ul>
-                </div>
-                <div class="col-xs-12 col-sm-4 col-md-4">
-                    <h5>Contact</h5>
-                    <ul class="list-unstyled quick-links">
-                        <li><a href="#"><i class="fa fa-angle-double-right"></i>Contact Us</a></li>
-
-                    </ul>
-                </div>
-                <div class="col-xs-12 col-sm-4 col-md-4">
-                    <h5>Legal</h5>
-                    <ul class="list-unstyled quick-links">
-                        <li><a href="#"><i class="fa fa-angle-double-right"></i>Privacy policy</a></li>
-                        <li><a href="cookies"><i class="fa fa-angle-double-right"></i>Cookies</a></li>
-
-
-                    </ul>
-                </div>
+                </ul>
             </div>
-            <hr class="my-4" >
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
-                    <ul class="list-unstyled list-inline social text-center">
-                        <li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
-                        <li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
-                        <li class="list-inline-item"><a href="#"><i class="fa fa-instagram"></i></a></li>
-                        <li class="list-inline-item"><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                        <li class="list-inline-item"><a href="#" target="_blank"><i class="fa fa-envelope"></i></a></li>
-                    </ul>
-                </div>
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <h5>Contact</h5>
+                <ul class="list-unstyled quick-links">
+                    <li><a href="#"><i class="fa fa-angle-double-right"></i>Contact Us</a></li>
+
+                </ul>
             </div>
-            <hr class="my-4" >
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-                    <p><u><a href="https://www.eysionic.com/"> Devolopped by Esyonic </a></u> </p>
-                    <p class="h6 right-reserved">&copy All right Reversed.<a class="text-green ml-2" href="https://www.eysionic.com/"
-                            target="_blank">Stefano Berberini</a></p>
-                </div>
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <h5>Legal</h5>
+                <ul class="list-unstyled quick-links">
+                    <li><a href="#"><i class="fa fa-angle-double-right"></i>Privacy policy</a></li>
+                    <li><a href="cookies"><i class="fa fa-angle-double-right"></i>Cookies</a></li>
+
+
+                </ul>
             </div>
         </div>
-    </section>
+        <hr class="my-4">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+                <ul class="list-unstyled list-inline social text-center">
+                    <li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href="#"><i class="fa fa-instagram"></i></a></li>
+                    <li class="list-inline-item"><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                    <li class="list-inline-item"><a href="#" target="_blank"><i class="fa fa-envelope"></i></a></li>
+                </ul>
+            </div>
+        </div>
+        <hr class="my-4">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+                <p><u><a href="https://www.eysionic.com/"> Devolopped by Esyonic </a></u> </p>
+                <p class="h6 right-reserved">&copy All right Reversed.<a class="text-green ml-2" href="https://www.eysionic.com/" target="_blank">Stefano Berberini</a></p>
+            </div>
+        </div>
+    </div>
+</section>
 
 
 
@@ -59,38 +58,75 @@
 
 
 
-    <!-- End Footer-->
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <!-- Swiper JS -->
-    <script type="text/javascript" src="js/swiper.min.js"></script>
-    <!-- Initialize Swiper -->
-    <script>
-        AOS.init();
-        var swiper = new Swiper('.swiper-container', {
-            effect: 'coverflow',
-            grabCursor: true,
-            centeredSlides: true,
-            slidesPerView: 'auto',
-            coverflowEffect: {
-                rotate: 20,
-                stretch: 0,
-                depth: 300,
-                modifier: 4.5,
-                slideShadows: true,
-            },
-            pagination: {
-                el: '.swiper-pagination',
-            },
+<!-- End Footer-->
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<!--<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<!-- Swiper JS -->
+<script type="text/javascript" src="js/swiper.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+<script>
+    // Select all links with hashes
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function() {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
         });
-    </script>
+
+</script>
+<!-- Initialize Swiper -->
+<script>
+    AOS.init();
+    var swiper = new Swiper('.swiper-container', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+            rotate: 20,
+            stretch: 0,
+            depth: 300,
+            modifier: 4.5,
+            slideShadows: true,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
+
+</script>
 </body>
 
 </html>
