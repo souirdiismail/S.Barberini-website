70b85908c22385b0cbe156704af89518819d0cf1
Puthus <32844173+Puthus@users.noreply.github.com>
2018-10-27 20:00:06 +0100

    some stuff

--- PHP STUFF/stephano_M/footer.php
+++ PHP STUFF/stephano_M/footer.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
     <section id="footer">
-      <hr class="my-4" >
+    	<hr class="my-4" >
         <div class="container">
             <div class="row text-center text-xs-center text-sm-left text-md-left">
                 <div class="col-xs-12 col-sm-4 col-md-4">
