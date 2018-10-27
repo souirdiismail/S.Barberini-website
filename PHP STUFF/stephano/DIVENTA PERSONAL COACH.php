0ce1483694869369fd6f5e4e4bfbadb05687e15c
Puthus <32844173+Puthus@users.noreply.github.com>
2018-10-27 17:26:12 +0100

    PHP STUFF

---
+++ PHP STUFF/stephano/DIVENTA PERSONAL COACH.php
@@ -0,0 +1,83 @@
+<?php require "header.php"?>
+
+    <section class="content container">
+        <div class="profile-photo">
+            <span class="card-img">
+                <img src="Assets\stefano coach.png" alt="" class="portraits">
+                <img src="Assets\Rectangle.png" alt="" class="stroke">
+            </span>
+        </div><br><br>
+        <div class="text">
+            <article>
+                Vuoi lavorare da casa ed essere libero?
+                <br><br>
+                Scopri come diventare Personal Coach Herbalife, aiutando le persone a raggiungere i propri obiettivi di
+                benessere!
+                <br><br>
+                Se sei stanco di lavorare per qualcun altro ed essere pagato il minimo, stanco di dover chiedere 1 ora
+                di permesso (che ti può essere NEGATO) per portare tuo figlio dal pediatra.
+                <br><br>
+                Stanco di realizzare i sogni di qualcun altro…continua a leggere questa pagina….ho una soluzione
+                concreta per te.
+                <br><br>
+                Vorresti sapere come anche tu, come me, potresti:
+                <br><br>
+                lavorare con Herbalife e diventare un PERSONAL COACH per avere un Guadagno EXTRA iniziando part time,
+                con orari flessibili cosi da riuscire a gestire tutti i tuoi impegni familiari iniziando la tua
+                attività indipendente con Herbalife, oppure
+                <br><br>
+                Iniziare un vero e proprio percorso di CARRIERA PROFESSIONALE con eccellenti prospettive di crescita
+                personale e guadagno?
+                <br><br>
+                Per cominciare a LAVORARE SUBITO IN HERBALIFE CON NOI e diventare un PERSONAL COACH, compila il modulo
+                qui sotto, ti riichiamerò entro 24 ore per spiegarti i dettagli e le scuole che ti offriremo (sia
+                online che offline) per diventare PERSONAL COACH Herbalife.
+            </article>
+        </div>
+    </section>
+    <!--Contact Us-->
+    <section class="contact text-center">
+        <div class="container">
+            <div class="content_contact ">
+                <br><br>
+                <h1>Contact Us</h1>
+                <div class="">
+                    <form>
+                        <div class="form-row form_contact">
+                            <div class="form-group col-md-6 contact_coach">
+                                <input type="text" class="form-control" id="inputname4" data-num="1" placeholder="Full Name">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <input type="email" class="form-control" id="inputEmail4" data-num="1" placeholder="Email">
+                            </div>
+
+                            <div class="form-group col-md-12">
+                                <input type="text" class="form-control" id="inputsubject" data-num="1" placeholder="Subject">
+                            </div>
+                            <div class="form-group col-md-12">
+                                <textarea name="name" rows="8" cols="80" class="form-control" data-num="1" id="inputmessage"
+                                    placeholder="Message"></textarea>
+                            </div>
+                            <br>
+                            <ul class="agreement">
+                                <li> <label class="switch">
+                                        <input type="checkbox">
+                                        <span class="slider round"></span>
+                                    </label></li>
+                                <li class="agree_text">
+                                    I Agree for condition.
+                                </li>
+                                <li class="li_button">
+                                    <button type="submit" class="btn btn_contact_coach">Submit</button>
+                                </li>
+                            </ul>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!-- End Contact Us-->
+
+<?php require "footer.php"?>
---
+++ PHP STUFF/stephano/header.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Lato font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+    <!-- AOS scrolling animation -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="icon" href="../../../../favicon.ico">
+    <link rel="stylesheet" type="text/css" href="swiper.min.css" />
+    <link rel="stylesheet" href="css/style.css">
+    <title></title>
+</head>
+
+<body>
+    <!-- NAVBAR -->
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark container col-12">
+            <a class="navbar-brand" href="#"><img src="assets/c24 logo dark.png" class="logo" alt="..."></a>
+            <button class="navbar-toggler navbuton" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+                        <path stroke="rgba(27,86,138,1)" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10"
+                            d="M4 7h22M4 15h22M4 23h22" /></svg>
+                </span>
+            </button>
+            <div class=" collapse navbar-collapse customnav" id="navbarNavDropdown">
+                <ul class="navbar-nav justify-content-end col-12">
+                    <li class="nav-item mx-2 current">
+                        <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+                        <svg class="line" viewBox="0 0 40 1" preserveAspectRatio="none">
+                            <path d="M0 1 L40 1" />
+                            <path d="M0 1 L40 1" class="focusedline" />
+                        </svg>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link" href="Prodotto.php">Prodotto</a>
+                        <svg class="line" viewBox="0 0 40 1" preserveAspectRatio="none">
+                            <path d="M0 1 L40 1" />
+                            <path d="M0 1 L40 1" class="focusedline" />
+                        </svg>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link" href="Video.php">Video</a>
+                        <svg class="line" viewBox="0 0 40 1" preserveAspectRatio="none">
+                            <path d="M0 1 L40 1" />
+                            <path d="M0 1 L40 1" class="focusedline" />
+                        </svg>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link" href="chi sono.php">Chi sono</a>
+                        <svg class="line" viewBox="0 0 40 1" preserveAspectRatio="none">
+                            <path d="M0 1 L40 1" />
+                            <path d="M0 1 L40 1" class="focusedline" />
+                        </svg>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link" href="DIVENTA PERSONAL COACH.php">Diventa personal coach</a>
+                        <svg class="line" viewBox="0 0 40 1" preserveAspectRatio="none">
+                            <path d="M0 1 L40 1" />
+                            <path d="M0 1 L40 1" class="focusedline" />
+                        </svg>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link" href="#">CONTATTO SULLA</a>
+                        <svg class="line" viewBox="0 0 40 1" preserveAspectRatio="none">
+                            <path d="M0 1 L40 1" />
+                            <path d="M0 1 L40 1" class="focusedline" />
+                        </svg>
+                    </li>
+                    <li class="nav-item mx-2 bordered">
+                        <a class="nav-link" href="#">Il nostro negozio</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    <div class="big-title" style="<?php if($_SERVER['PHP_SELF'] =="/index.php")echo 'display:none;' ?>">
+        <span class="title">
+            <?php
+                echo strtoupper( trim(trim($_SERVER['PHP_SELF'],".php"),"/"));
+            ?>
+        </span>
+    </div>
+    <!-- END NAVBAR -->
