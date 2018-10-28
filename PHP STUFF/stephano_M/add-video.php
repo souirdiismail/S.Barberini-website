6b4a6cd86e5c2b617f03acab53c37397f4b8014f
Atheem <36827002+Atheem@users.noreply.github.com>
2018-10-28 20:02:10 +0000

    up

---
+++ PHP STUFF/stephano_M/add-video.php
@@ -0,0 +1,30 @@
+<?php require "header.php"?>
+
+
+<div class="container">
+                <form action="" method="post" name="form">
+                    <div class="form-row form_contact">
+                        <div class="form-group col-md-6">
+                            <input type="text"  class="form-control" placeholder="Title">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <input type="file"  class="form-control"  placeholder="Card Image">
+                        </div>
+
+                        <div class="form-group col-md-12">
+                        <textarea name="descp" rows="8" cols="80" class="form-control" 
+                                placeholder="Descripion"></textarea>
+                        </div>
+                        <div class="form-group col-md-12">
+                            <textarea name="content" rows="8" cols="80" class="form-control" 
+                                placeholder="Content"></textarea>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <input type="text"  class="form-control" placeholder="Url">
+                        </div>
+                        <br>
+                        <button type="submit" class="btn btn_contact col-md-12">Submit</button>
+                    </div>
+                </form>
+            </div>
+<?php require "footer.php"?>
